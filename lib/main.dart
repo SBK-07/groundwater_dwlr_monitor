b51018b116c4e71110f5b63b65e436cf01596d23
Srinivasan1716 <srinivasan2470047@ssn.edu.in>
2025-09-20 22:51:10 +0530

    Srini's first move

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Groundwater Monitor',
+      title: 'Groundwater Monitor  using DWLR stations.  ",
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomeScreen(),
     );
